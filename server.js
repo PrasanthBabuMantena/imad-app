e497f62759220bd4e691950b3e518b104199b0c4
PrasanthBabuMantena <30702556+PrasanthBabuMantena@users.noreply.github.com>
2017-08-06 02:30:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/hostel.html',function(req,res){
+    res.sendFile(path.join(__dirname,'ui','hostel.html'));
+})
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
