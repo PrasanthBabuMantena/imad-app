6652121b5c99fcfd8bb9a7e544126da612bcd67c
PrasanthBabuMantena <30702556+PrasanthBabuMantena@users.noreply.github.com>
2017-08-12 09:08:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+counter=0;
+var button=document.getElementById("count");
+var span=getElementById("counter");
+button.onclick =function(){
+    counter=counter+1;
+    span.InnerHTML=counter.toString();
+}
