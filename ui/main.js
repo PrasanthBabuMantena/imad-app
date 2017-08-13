237ce74e7c97dcc196e23f4fef9c3d7d352f6dab
PrasanthBabuMantena <30702556+PrasanthBabuMantena@users.noreply.github.com>
2017-08-13 14:56:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,26 +2,19 @@ console.log("loaded");
 window.alert("Welcome to ECE official");
 
 var button=document.getElementById('like');
-button.onclick = function () {
-    
+button.onclick = function () 
+{
     var request=new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readystate===XMLHttpRequest.DONE)
         {
-
-           
-            if(request.status===200)
+            if(request.status===404)
             {
                 var counter=request.responseText;
                 var span=document.getElementById('count');
                 span.innerHTML=245543;
-            
-        
             }
-        
-            
         }
-    
     };
      
         
