44dd0981122dd3b9a456bb0745eec4424f39bfab
PrasanthBabuMantena <30702556+PrasanthBabuMantena@users.noreply.github.com>
2017-08-13 20:40:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log("loaded");
- var span=document.getElementById('count');
-                span.innerHTML=counter.toString();
+
+
+
 var button=document.getElementById('like');
 button.onclick = function () 
 {
@@ -60,6 +61,27 @@ button.onclick = function ()
 };
 
 
+
+function () 
+{
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+     
+        
+        request.open('GET','http://prasanthbabupadma.imad.hasura-app.io/counter',true);
+        request.send();
+   
+};
    
    
 
