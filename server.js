9600590253b7611d160109f305b161a31c4be760
PrasanthBabuMantena <30702556+PrasanthBabuMantena@users.noreply.github.com>
2017-08-13 17:42:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/counter',function (req,res){
 });
 var names=[];
 app.get('/submit-name/:name',function(req,res){
-  var name=req.params.name;
+  var name=req.query.name;
 names.push(name);
 //JSON IS jAVA SCRIPT OBJECT NOTATION
 res.send(JSON.stringify(names));
