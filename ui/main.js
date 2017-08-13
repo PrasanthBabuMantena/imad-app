a99d2f086312e5f5d94812ab5c14e06615a9d024
PrasanthBabuMantena <30702556+PrasanthBabuMantena@users.noreply.github.com>
2017-08-13 15:00:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,13 @@
 console.log("loaded");
 window.alert("Welcome to ECE official");
 
+
 var button=document.getElementById('like');
 button.onclick = function () 
 {
     var request=new XMLHttpRequest();
     request.onreadystatechange = function(){
-        if(request.readystate===XMLHttpRequest.DONE)
+        if(request.readyState===XMLHttpRequest.DONE)
         {
             if(request.status===404)
             {
