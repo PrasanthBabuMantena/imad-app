f69bb3f63a1e0e8cbbc073ec848679b8cb36697d
PrasanthBabuMantena <30702556+PrasanthBabuMantena@users.noreply.github.com>
2017-08-13 11:30:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,7 @@ button.onclick = function () {
     request.onreadystatechange = function(){
         if(request.readystate===XMLHttpRequest.DONE)
         {
+            var a=10;
             if(request.status===200)
             {
                 var a=request.responseText;
