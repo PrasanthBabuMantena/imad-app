8392595b375e3f3f90253f0302078e5df4b38fd9
PrasanthBabuMantena <30702556+PrasanthBabuMantena@users.noreply.github.com>
2017-08-20 17:49:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -38,7 +38,11 @@ if(err)
      {
 res.status(500).send("Something went wrong");
       }
-else{
+     else if(result.rows.length!==0)
+     {
+         res.status(400).send("User not found");
+     }
+     else{
 var li=['<tr><th>Station</th><th>Arrival Time</th><th>Departure Time</th></tr>'];
 var n=result.rows.count;
 console.log(n);
